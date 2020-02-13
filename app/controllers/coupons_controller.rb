9e77711e7ba3f65f597514e095cbf41244034097
Daniel Matney <danieljmatney@gmail.com>
2020-02-13 17:47:17 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -18,7 +18,7 @@ class CouponsController < ApplicationController
     @coupon.coupon_code = params[:coupon_code]
     @coupon.store = params[:store]
     @coupon.save
-    redirect_to post_path(@coupon)
+    redirect_to coupon_path(@coupon)
   end
   
   
